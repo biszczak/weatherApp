9df729c45ad3b3e0a41a41941951e1b0f85f0c70
biszczak <dbiszczak@gmail.com>
2020-03-05 13:23:39 +0100

    Updates

---
+++ precache-manifest.6ad504d6bb7c43a4eb1dbd0a2be5f14b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ecbe8be8a61a8b9aba746d768681323",
+    "url": "/weatherApp/static/media/weather_30.0ecbe8be.svg"
+  },
+  {
+    "revision": "7188140e287fface7087",
+    "url": "/weatherApp/static/css/main.757fb6a8.chunk.css"
+  },
+  {
+    "revision": "b0ae699ea3cc63fef9c8",
+    "url": "/weatherApp/static/js/runtime~main.25533534.js"
+  },
+  {
+    "revision": "24e096ef3d66b8f190c6",
+    "url": "/weatherApp/static/js/2.a60ca3ce.chunk.js"
+  },
+  {
+    "revision": "2fd75af0ca81c79f882df1c3f941a97e",
+    "url": "/weatherApp/static/media/weather_1.2fd75af0.svg"
+  },
+  {
+    "revision": "811ba74fb75c28f163e6d4d22ccfa263",
+    "url": "/weatherApp/static/media/weather_6.811ba74f.svg"
+  },
+  {
+    "revision": "006e1fc760776c9bd9f90e57e4e5acaa",
+    "url": "/weatherApp/static/media/weather_23.006e1fc7.svg"
+  },
+  {
+    "revision": "79d1c380efa55c92746aaf0a4de8ce2c",
+    "url": "/weatherApp/static/media/weather_35.79d1c380.svg"
+  },
+  {
+    "revision": "7188140e287fface7087",
+    "url": "/weatherApp/static/js/main.ed27dfcf.chunk.js"
+  },
+  {
+    "revision": "1075e6dd8bb50234cbd659599210cd12",
+    "url": "/weatherApp/static/media/weather_2.1075e6dd.svg"
+  },
+  {
+    "revision": "c62a7931606dca69f0f9e7ebdb92a763",
+    "url": "/weatherApp/static/media/weather_5.c62a7931.svg"
+  },
+  {
+    "revision": "ea882d2c136a74f3bb6fd05b91985f39",
+    "url": "/weatherApp/static/media/weather_3.ea882d2c.svg"
+  },
+  {
+    "revision": "d5992ae9b9209524b1922ac3ea3ed8ae",
+    "url": "/weatherApp/static/media/weather_4.d5992ae9.svg"
+  },
+  {
+    "revision": "bd5ad00507f12bdf45a900575db2e1ed",
+    "url": "/weatherApp/static/media/weather_7.bd5ad005.svg"
+  },
+  {
+    "revision": "0a3eb997f8e2ca70a4078bf7cec353c2",
+    "url": "/weatherApp/static/media/weather_8.0a3eb997.svg"
+  },
+  {
+    "revision": "5e107ccc584f45c2e940632ad7ba5990",
+    "url": "/weatherApp/index.html"
+  }
+];
